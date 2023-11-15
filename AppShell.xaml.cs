f27af6cdf988fe91a217cfdd4c8220b7e738fb1e
admin <generic1290@protonmail.com>
2023-11-14 20:46:58 -0600

    first commit

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace AudioRecord;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using Plugin.Maui.Audio;
+
+namespace AudioRecord;
+
+public partial class MainPage : ContentPage
+{
+
+	readonly IAudioManager _audioManager;
+	readonly IAudioRecorder _audioRecorder;
+
+	public MainPage(IAudioManager audioManager)
+	{
+		InitializeComponent();
+
+		_audioManager = audioManager;
+		_audioRecorder = audioManager.CreateRecorder();
+	}
+
+	private async void OnCounterClicked(object sender, EventArgs e)
+	{
+		if (await Permissions.RequestAsync<Permissions.Microphone>() != PermissionStatus.Granted)
+		{
+			// informar usuario
+			return;
+		}
+
+		if( !_audioRecorder.IsRecording ) 
+		{
+			await _audioRecorder.StartAsync();
+		}
+
+		else
+		{
+			var recordedAudio = await _audioRecorder.StopAsync();
+
+			var player = AudioManager.Current.CreatePlayer(recordedAudio.GetAudioStream());
+			player.Play();
+		}
+	}
+}
+
